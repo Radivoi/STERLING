c148bde7fe56ce19d954df05a68dcfef8255721f
Radivoi <michaelradivoi@gmail.com>
2025-07-27 02:33:44 -0400

    Various

--- _site/js/freelancer.js
+++ _site/js/freelancer.js
@@ -6,14 +6,20 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    var target = $($anchor.attr('href'));
+
+    if (target.length) {
+      $('html, body').stop().animate({
+        scrollTop: target.offset().top - 40 // 👈 offset by 40px
+      }, 1500, 'easeInOutExpo');
+    }
+
+    event.preventDefault();
+  });
 });
+
 
 // Floating label headings for the contact form
 $(function() {
